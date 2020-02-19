6c25af3e3c50f39d55ab7ec45900022974de9408
Benjamin Klaus <klaus@technikum-wien.at>
2020-02-19 16:07:15 +0100

    re-color and translate shown tags based on data from REST-API on window load

--- qa-content/qa-vue.js
+++ qa-content/qa-vue.js
@@ -6,3 +6,15 @@ function qa_init_vue_tag_selector(baseElem) {
         },
     })
 }
+
+window.addEventListener('load', function () {
+    $.get('https://tags.asterics-foundation.org:4000/tags', null, function(tags) {
+        $('.qa-tag-link').each((index, element) => {
+            let elem = $(element);
+            let id = elem.text();
+            elem.text(tagUtil.getLabel(id, tags));
+            elem.css(tagUtil.getColorStyleObject(id, tags));
+            elem.show();
+        });
+    });
+});
