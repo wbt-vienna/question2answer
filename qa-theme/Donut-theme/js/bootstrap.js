b868d17f4f6dfd3bd92a6598232653082c0b9bb2
Benjamin Klaus <klaus@technikum-wien.at>
2020-05-11 14:46:33 +0200

    disable bootstrap-titles since they are not read seamlessly by screenreader

--- qa-theme/Donut-theme/js/bootstrap.js
+++ qa-theme/Donut-theme/js/bootstrap.js
@@ -1443,10 +1443,10 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
   }
 
   Tooltip.prototype.fixTitle = function () {
-    var $e = this.$element
+    /*var $e = this.$element
     if ($e.attr('title') || typeof ($e.attr('data-original-title')) != 'string') {
       $e.attr('data-original-title', $e.attr('title') || '').attr('title', '')
-    }
+    }*/
   }
 
   Tooltip.prototype.hasContent = function () {
