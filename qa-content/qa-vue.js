9581429191b73ea41f4ed17f348ec5b49d609887
Benjamin Klaus <klaus@technikum-wien.at>
2020-05-12 12:20:46 +0200

    made code failsafe if no tags available

--- qa-content/qa-vue.js
+++ qa-content/qa-vue.js
@@ -11,9 +11,11 @@ window.addEventListener('load', function () {
     let tags = JSON.parse(qa_all_tags);
     $('.qa-tag-link').each((index, element) => {
         let elem = $(element);
-        let id = elem.text();
-        elem.text(tagUtil.getLabel(id, tags));
-        elem.css(tagUtil.getColorStyleObject(id, tags));
+        if (tags) {
+            let id = elem.text();
+            elem.text(tagUtil.getLabel(id, tags));
+            elem.css(tagUtil.getColorStyleObject(id, tags));
+        }
         elem.show();
     });
     $('h1').each((i, element) => {
@@ -21,7 +23,7 @@ window.addEventListener('load', function () {
         let searchString = 'Fragen mit Tag "'
         let text = elem.text();
         let index = text.indexOf(searchString);
-        if (index !== -1) {
+        if (index !== -1 && tags) {
             let id = text.substring(17, text.lastIndexOf('"'));
             elem.text(searchString + tagUtil.getLabel(id, tags) + '"');
         }
