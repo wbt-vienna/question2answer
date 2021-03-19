a0805c4cfccd2844bc9bf340d8046ee7848651dc
Benjamin Klaus <klaus@technikum-wien.at>
2021-03-19 12:55:39 +0100

    added Matomo analytics

--- qa-content/qa-load.js
+++ qa-content/qa-load.js
@@ -9,3 +9,20 @@ window.addEventListener('load', function () {
         }, 300);
     }
 });
+
+initMatomoAnalytics();
+function initMatomoAnalytics() {
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(["setDoNotTrack", true]);
+    _paq.push(["disableCookies"]);
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+        var u="//analytics.wbt.wien/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '4']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    })();
+}
